d1dd1e0eb0469b931dc6faf9265914a6aac28d8c
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-02-28 17:09:17 +0700

    DuongLTH update source code

--- src/main/java/com/codegym/service/impl/CommodityService.java
+++ src/main/java/com/codegym/service/impl/CommodityService.java
@@ -1,6 +1,5 @@
 package com.codegym.service.impl;
 
-import com.codegym.model.SalesReport.ISalesReport;
 import com.codegym.model.commodity.Commodity;
 import com.codegym.repository.ICommodityRepository;
 import com.codegym.service.ICommodityService;
@@ -15,12 +14,12 @@ public class CommodityService implements ICommodityService {
     /**
      * Create by : DuongLTH
      * Date create 27/02/2023
-     * @param QRCode
+     * @param qrCode
      */
 
     @Override
-    public Commodity findByQRCode(String QRCode) {
-        return commodityRepository.findByQRCode(QRCode);
+    public Commodity findByQRCode(String qrCode) {
+        return commodityRepository.findByQRCode(qrCode);
     }
 
 
