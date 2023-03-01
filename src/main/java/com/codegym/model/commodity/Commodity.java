99bdeb0e6dc19a394820ba67f22f60ae8a43c12d
unknown <tranhautanvlog.vn@gmail.com>
2023-03-01 13:59:10 +0700

    tanTH update

--- src/main/java/com/codegym/model/commodity/Commodity.java
+++ src/main/java/com/codegym/model/commodity/Commodity.java
@@ -4,14 +4,10 @@ import javax.persistence.*;
 
 @Entity
 public class Commodity {
-<<<<<<< HEAD
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-=======
 
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
->>>>>>> origin/develop
     private Integer id;
     private String name;
     private String cpu;
@@ -24,21 +20,19 @@ public class Commodity {
     private String guarantee;
     private String origin;
     private String description;
-<<<<<<< HEAD
     private String codeQr;
     private Integer quantity;
     private Boolean flagDelete;
 
     @ManyToOne
     private Trademark trademark;
-=======
+
     private String codeQR;
-    private int quantity;
-    private boolean flagDelete;
+  /*  private int quantity;
+    private boolean flagDelete;*/
 
 
     private int interestRate = 10;
->>>>>>> origin/develop
 
     public Commodity() {
 
@@ -164,7 +158,6 @@ public class Commodity {
         this.quantity = quantity;
     }
 
-<<<<<<< HEAD
     public Boolean getFlagDelete() {
         return flagDelete;
     }
@@ -180,7 +173,7 @@ public class Commodity {
     public void setTrademark(Trademark trademark) {
         this.trademark = trademark;
     }
-=======
+
     public boolean isFlagDelete() {
         return flagDelete;
     }
@@ -188,5 +181,5 @@ public class Commodity {
     public void setFlagDelete(boolean flagDelete) {
         this.flagDelete = flagDelete;
     }
->>>>>>> origin/develop
+
 }
