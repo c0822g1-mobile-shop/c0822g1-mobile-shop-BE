a469191fef0ab6909bc19b8cd95178e9dc28226c
congtranc0722g1 <trantancongall@gmail.com>
2023-03-03 13:32:13 +0700

    CongTT create and update supplier

--- src/main/java/com/codegym/controller/supplierController/SupplierController.java
+++ src/main/java/com/codegym/controller/supplierController/SupplierController.java
@@ -35,7 +35,7 @@ public class SupplierController {
      */
     @GetMapping("/list")
     private ResponseEntity<?> showList(@RequestParam(defaultValue = "", required = false) String search,
-                                       @PageableDefault(size = 3) Pageable pageable) {
+                                       @PageableDefault(size = 5) Pageable pageable) {
         System.out.println(search);
         Page<List<Supplier>> supplierListDtoList = supplierService.showList(search, pageable);
         return new ResponseEntity<>(supplierListDtoList, HttpStatus.OK);
@@ -55,8 +55,8 @@ public class SupplierController {
      * Created By: CongTT
      * Date created: 27/02/2023
      * Function: create supplier
-     * @Param: supplierDto
-     * Return: HttpStatus.BAD_REQUEST if result is error or HttpStatus.CREATED if result is not error.
+     * @param: supplierDto
+     * @return: HttpStatus.BAD_REQUEST if result is error or HttpStatus.CREATED if result is not error.
      */
 
     @PostMapping("/add")
@@ -89,8 +89,8 @@ public class SupplierController {
      * Created By: CongTT
      * Date created: 27/02/2023
      * Function: find supplier by id
-     * @Param: id
-     * Return: HttpStatus.OK if result is not error.
+     * @param: id
+     * @return: HttpStatus.OK if result is not error.
      */
 
     @GetMapping("/supplier")
@@ -108,8 +108,8 @@ public class SupplierController {
      * Created By: CongTT
      * Date created: 27/02/2023
      * Function: update supplier
-     * Param: supplierDto
-     * Return: HttpStatus.BAD_REQUEST if result is error or HttpStatus.OK if result is not error.
+     * @param: supplierDto
+     * @return: HttpStatus.BAD_REQUEST if result is error or HttpStatus.OK if result is not error.
      */
 
     @PutMapping("/update")
