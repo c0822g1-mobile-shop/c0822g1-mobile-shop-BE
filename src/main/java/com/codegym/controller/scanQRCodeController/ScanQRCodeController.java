12cbf327dba0ccd5cbcbd6d78bf0b62eb0770796
letrunghaiduong <letrunghaiduong260701@gmail.com>
2023-03-05 15:16:31 +0700

    DuongLTH update source code

--- src/main/java/com/codegym/controller/scanQRCodeController/ScanQRCodeController.java
+++ src/main/java/com/codegym/controller/scanQRCodeController/ScanQRCodeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/scanQR")
 @CrossOrigin
 public class ScanQRCodeController {
+
     @Autowired
     private ICommodityService commodityService;
 
